f8d2564ff360073a02d4b4dd399b42eee267a47f
Natalia Jaworek <natalia_wiktor@o2.pl>
2020-01-22 23:33:26 +0100

    initial commit

---
+++ main.js
@@ -0,0 +1,8 @@
+alert ('Cześć :) ja tu programuje, a co u Ciebie?');
+console.log ('siema');
+const name = 'Natalia';
+const age = '31';
+console.log(name);
+console.log(age);
+
+console.log(`Nazywam się ${name} i mam ${age} lat`);
