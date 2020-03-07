7ff850df627e7e4f7b6b4d33d14b38ae2411cadd
Natalia Jaworek <natalia_wiktor@o2.pl>
2020-03-07 22:44:25 +0100

    feat: homepage with gradient

--- main.js
+++ main.js
@@ -1,16 +1,35 @@
 alert ('Cześć :) ja tu programuje, a co u Ciebie?');
-console.log ('siema');
-const name = 'Natalia';
-const age = '31';
-console.log(name);
-console.log(age);
-
-console.log(`Nazywam się ${name} i mam ${age} lat`);
 
 const div = document.querySelectorAll(`div`);
 
 console.log(div);
 
-const footer = document.querySelector(`footer`);
+const footer = document.querySelector(`.footer`);
 
-consolw.log(footer);
+console.log(footer);
+
+const header = document.querySelector(`.article__header`)
+
+console.log(header);
+    
+const paragraphs = document.querySelectorAll(`p`);
+
+console.log(paragraphs);
+
+const heading = document.querySelector('.article__heading');
+
+console.log(heading.innerHTML);
+
+heading.innerHTML = 'Moja strona domowa';
+
+const x = document.querySelector('.article__object');
+
+console.log(x.innerHTML);
+
+x.innerHTML = 'Nowy obiekt dodany'
+
+console.log('Hello');
+const name = 'Natalia';
+const age = '31';
+
+console.log('Nazywam się ${name} i mam ${age} lat, a Ty?');
